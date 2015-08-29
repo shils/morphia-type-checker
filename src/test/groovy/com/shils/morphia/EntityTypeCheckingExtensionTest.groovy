c5da9cd0baf7161652f37d00be75f4aff6e2031d
Shil S <shil.sinha@gmail.com>
2015-08-28 23:08:33 -0300

    modified EntityTypeCheckingExtension tests to include descending modifiers in deprecated @Index usages

--- src/test/groovy/com/shils/morphia/EntityTypeCheckingExtensionTest.groovy
+++ src/test/groovy/com/shils/morphia/EntityTypeCheckingExtensionTest.groovy
@@ -85,7 +85,7 @@ class EntityTypeCheckingExtensionTest extends GroovyShellTestCase {
 
     message = shouldFail {
       shell.evaluate '''
-        @Indexes(@Index('aInt'))
+        @Indexes(@Index('-aInt'))
         @Entity
         class A {
           int anInt
@@ -108,7 +108,7 @@ class EntityTypeCheckingExtensionTest extends GroovyShellTestCase {
 
     message = shouldFail {
       shell.evaluate '''
-        @Indexes(@Index('anInt, anString'))
+        @Indexes(@Index('-anInt, anString'))
         @Entity
         class A {
           int anInt
